9eb226d08b9189ee9e847d6a25913e0c0dd63c4e
Luke Miller <lamiller516@gmail.com>
2022-03-23 18:00:27 -0400

    Added search bar

--- src/components/HeroIndex.js
+++ src/components/HeroIndex.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Form, Input, Label, Button } from "reactstrap";
+const baseURL = `https://superheroapi.com/api/${process.env.REACT_ADMIN_API_KEY}`;
 
-const heroIndex = () => {
+const HeroIndex = () => {
+    const [ searchTerm, setSearchTerm ] = useState("");
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        console.log({ ...searchTerm, [name]: value });
+        setSearchTerm({ ...searchTerm, [name]: value });
+    };
+
     
     return(
-        <div>
-            Test
+        <div className="app">
+            <div className="controls">
+                <Form>
+                    <Label for="search">Search for A Character</Label>
+                    <Input type="text" 
+                    name="searchTerm" 
+                    id="search" 
+                    className="search" 
+                    placeholder="Batman" 
+                    value={searchTerm} 
+                    onChange={handleInputChange} required />
+                    <Button
+                    color="primary">Search</Button>
+                </Form>
+            </div>            
         </div>
     )
 }
 
-export default heroIndex;
+export default HeroIndex;
